eb83ca24e0e154ad627416e23c61bd8d75cbbf92
trupa7 <35124558+trupa7@users.noreply.github.com>
2018-03-09 10:03:10 -0500

    topics delete

--- server/routes/api.js
+++ server/routes/api.js
@@ -67,32 +67,24 @@ router.put('/meetings/topics/:id',function(req,res,next){
         });
     }
 });
-
-
 });
 
-//delete new topics
-router.delete('/meetings/topics/:id',function(req,res,next){
-  Meeting.findOne({_id:req.params.id}, function(error, meeting){
-    if(error){
-        res.json(error);
-    }
-    else if(meeting == null){
-        res.json('no such meeting!')
-    }
-    else{
-      console.log(req.body);
-        meeting.topics.pullAll( req.body);
-        meeting.save( function(error, data){
-            if(error){
-                res.json(error);
-            }
-            else{
-                res.json(data);
-            }
-        });
-    }
-});
+//delete topics
+router.delete('/meetings/topics/:id',function(req,res,next){  console.log(req.body);
+  Meeting.findOneAndUpdate(
+
+         { _id: req.params.id },
+         { $pull: { topics: req.body}},
+         // !!!! note the { 'new': true } option
+         { 'new': true },
+         function(error, data) {
+           if(error){
+               res.json(error);
+           }
+           else{
+               res.json(data);
+           }
+         });
 });
 
 module.exports=router;
