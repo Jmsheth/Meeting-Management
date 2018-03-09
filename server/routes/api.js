93a852af5c1a0cb42cb162efc40574956c1110bd
trupa7 <35124558+trupa7@users.noreply.github.com>
2018-03-09 10:15:19 -0500

    api

--- server/routes/api.js
+++ server/routes/api.js
@@ -43,7 +43,6 @@ router.delete("/meetings/:id", function(req, res, next) {
 });
 
 //push new topics
-<<<<<<< HEAD
 router.put('/meetings/topics/:id',function(req,res,next){
  Meeting.findOne({_id:req.params.id}, function(error, meeting){
     if(error){
@@ -63,27 +62,6 @@ router.put('/meetings/topics/:id',function(req,res,next){
                 res.json(data);
             }
         });
-=======
-router.post("/meetings/topics/:id", function(req, res, next) {
-  Meeting.findOne({ _id: req.params.id }, function(error, meeting) {
-    if (error) {
-      res.json(error);
-    } else if (meeting == null) {
-      res.json("no such meeting!");
-    } else {
-      console.log(req.params.topics);
-      Meeting.topics.push({ t_name: "trupal" });
-      Meeting.save(function(error, data) {
-        if (error) {
-          res.json(error);
-        } else {
-          res.json(data);
-        }
-      });
->>>>>>> 400c2ba1e2394d02b1cc989eb05f541570b71abc
-    }
-  });
-});
 
 //_______________________________________Task__________________________________
 
@@ -104,24 +82,7 @@ router.post("/tasks", function(req, res, next) {
     .catch(next);
 });
 
-<<<<<<< HEAD
-//delete topics
-router.delete('/meetings/topics/:id',function(req,res,next){  console.log(req.body);
-  Meeting.findOneAndUpdate(
 
-         { _id: req.params.id },
-         { $pull: { topics: req.body}},
-         // !!!! note the { 'new': true } option
-         { 'new': true },
-         function(error, data) {
-           if(error){
-               res.json(error);
-           }
-           else{
-               res.json(data);
-           }
-         });
-=======
 //update
 router.put("/tasks/:id", function(req, res, next) {
   Task.findByIdAndUpdate({ _id: req.params.id }, req.body).then(function() {
@@ -136,7 +97,6 @@ router.delete("/tasks/:id", function(req, res, next) {
   Task.findByIdAndRemove({ _id: req.params.id }).then(function(task) {
     res.send(task);
   });
->>>>>>> 400c2ba1e2394d02b1cc989eb05f541570b71abc
 });
 
 module.exports = router;
