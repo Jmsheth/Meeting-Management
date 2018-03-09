400c2ba1e2394d02b1cc989eb05f541570b71abc
Jay Sheth <jsheth@my.bridgeport.edu>
2018-03-09 01:31:35 -0500

    tasks
    initial api

---
+++ server/models/Task.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const Meeting = require("./meeting");
+//create tasks using mongodb
+
+const taskSchema = new Schema({
+  taskName: {
+    type: String,
+    required: [true, "Task name is required"]
+  },
+  taskParticipant: {
+    type: String,
+    required: [true, "One Participant is required"]
+  },
+  dueDate: {
+    type: Date,
+    requied: [false]
+  },
+  tags: {
+    type: String,
+    required: [true, "tag is required"]
+  },
+  notes: {
+    type: String,
+    required: [false]
+  }
+});
+
+const task = mongoose.model("task", taskSchema);
+module.exports = task;
--- server/models/meeting.js
+++ server/models/meeting.js
@@ -1,53 +1,58 @@
-const mongoose =require("mongoose");
-const Schema=mongoose.Schema;
-
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 //create employye schema @model
 
-const MeetingSchema=new Schema({
-  m_name:{
-    type:String,
-    required:[true,'Meeting Name is required'],
+const MeetingSchema = new Schema({
+  m_name: {
+    type: String,
+    required: [true, "Meeting Name is required"]
   },
-  m_tags:{
-    type:String,
-    required:[true,'tag is required']
+  m_tags: {
+    type: String,
+    required: [true, "tag is required"]
   },
-  m_participant:{
-    type:String,
-    required:[true,'Participant is required']
+  m_participant: {
+    type: String,
+    required: [true, "Participant is required"]
   },
-  from_date:{
-    type:Date,
-    required:[true,'From Date is required']
+  from_date: {
+    type: Date,
+    required: [true, "From Date is required"]
   },
-  to_date:{
-    type:Date,
-    required:[true,'To Date is required']
+  to_date: {
+    type: Date,
+    required: [true, "To Date is required"]
   },
-  location:{
-      type:String,
-      required:[true,'Locattion is required']
-    },
-    topics: [{
-         t_name: {
-           type:String,
-           required:[false]
-         },
-         notes: [{
-              n_name: {
-                type:String,
-                required:[false]
-              }
-          }],
-          decisons: [{
-               d_name: {
-                 type:String,
-                 required:[false]
-               }
-           }]
-     }]
+  location: {
+    type: String,
+    required: [true, "Locattion is required"]
+  },
+  topics: [
+    {
+      t_name: {
+        type: String,
+        required: [false]
+      },
+      notes: [
+        {
+          n_name: {
+            type: String,
+            required: [false]
+          }
+        }
+      ],
+      decisons: [
+        {
+          d_name: {
+            type: String,
+            required: [false]
+          }
+        }
+      ]
+    }
+  ]
 });
 
-const Meeting=mongoose.model('meeting',MeetingSchema);
-module.exports=Meeting;
+const Meeting = mongoose.model("meeting", MeetingSchema);
+module.exports = Meeting;
