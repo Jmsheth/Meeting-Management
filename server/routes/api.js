c3d0418aa4011abf92ac27bfe9fbb49ee7d868d0
trupa7 <35124558+trupa7@users.noreply.github.com>
2018-03-09 10:09:53 -0500

    l

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,26 +1,49 @@
+const express = require("express");
+const router = express.Router();
+const Meeting = require("../models/meeting");
+const Task = require("../models/Task");
 
 //get
+router.get("/meetings", function(req, res, next) {
+  Meeting.find({}).then(function(meeting) {
     res.send(meeting);
   });
 });
 
 //get by id
+router.get("/meetings/:id", function(req, res, next) {
+  Meeting.findOne({ _id: req.params.id }, req.body).then(function(meeting) {
+    res.send(meeting);
+  });
 });
 
 //insert
+router.post("/meetings", function(req, res, next) {
+  Meeting.create(req.body)
+    .then(function(meeting) {
+      res.send(meeting);
+    })
+    .catch(next);
 });
 
 //update
+router.put("/meetings/:id", function(req, res, next) {
+  Meeting.findByIdAndUpdate({ _id: req.params.id }, req.body).then(function() {
+    Meeting.findOne({ _id: req.params.id }, req.body).then(function(meeting) {
       res.send(meeting);
     });
   });
 });
 
 //delete
+router.delete("/meetings/:id", function(req, res, next) {
+  Meeting.findByIdAndRemove({ _id: req.params.id }).then(function(meeting) {
+    res.send(meeting);
   });
 });
 
 //push new topics
+<<<<<<< HEAD
 router.put('/meetings/topics/:id',function(req,res,next){
  Meeting.findOne({_id:req.params.id}, function(error, meeting){
     if(error){
@@ -40,10 +63,48 @@ router.put('/meetings/topics/:id',function(req,res,next){
                 res.json(data);
             }
         });
+=======
+router.post("/meetings/topics/:id", function(req, res, next) {
+  Meeting.findOne({ _id: req.params.id }, function(error, meeting) {
+    if (error) {
+      res.json(error);
+    } else if (meeting == null) {
+      res.json("no such meeting!");
+    } else {
+      console.log(req.params.topics);
+      Meeting.topics.push({ t_name: "trupal" });
+      Meeting.save(function(error, data) {
+        if (error) {
+          res.json(error);
+        } else {
+          res.json(data);
+        }
+      });
+>>>>>>> 400c2ba1e2394d02b1cc989eb05f541570b71abc
     }
-});
+  });
 });
 
+//_______________________________________Task__________________________________
+
+router.get("/tasks", function(req, res, next) {
+  Task.find({}).then(function(err, task) {
+    if (err) {
+      res.send(err);
+    }
+    res.send(task);
+  });
+});
+
+router.post("/tasks", function(req, res, next) {
+  Task.create(req.body)
+    .then(function(task) {
+      res.send(task);
+    })
+    .catch(next);
+});
+
+<<<<<<< HEAD
 //delete topics
 router.delete('/meetings/topics/:id',function(req,res,next){  console.log(req.body);
   Meeting.findOneAndUpdate(
@@ -60,5 +121,22 @@ router.delete('/meetings/topics/:id',function(req,res,next){  console.log(req.bo
                res.json(data);
            }
          });
+=======
+//update
+router.put("/tasks/:id", function(req, res, next) {
+  Task.findByIdAndUpdate({ _id: req.params.id }, req.body).then(function() {
+    Task.findOne({ _id: req.params.id }, req.body).then(function(task) {
+      res.send(task);
+    });
+  });
 });
 
+//delete
+router.delete("/tasks/:id", function(req, res, next) {
+  Task.findByIdAndRemove({ _id: req.params.id }).then(function(task) {
+    res.send(task);
+  });
+>>>>>>> 400c2ba1e2394d02b1cc989eb05f541570b71abc
+});
+
+module.exports = router;
