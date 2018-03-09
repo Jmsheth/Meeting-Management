6cded21e605eb90b66a399546f215b43ebe4c6e0
trupa7 <35124558+trupa7@users.noreply.github.com>
2018-03-09 11:03:33 -0500

    nested array update,delete,push

--- server/routes/api.js
+++ server/routes/api.js
@@ -47,44 +47,9 @@ router.delete("/meetings/:id", function(req, res, next) {
 //.................................topics---------------------------------------------------
 //push new topics
 
-router.put('/meetings/topics/:id',function(req,res,next){
- Meeting.findOne({_id:req.params.id}, function(error, meeting){
-    if(error){
-        res.json(error);
-    }
-    else if(meeting == null){
-        res.json('no such meeting!')
-    }
-    else{
-      console.log(req.body);
-        meeting.topics.push( req.body);
-        meeting.save( function(error, data){
-            if(error){
-                res.json(error);
-            }
-            else{
-                res.json(data);
-            }
-        });
-
 
 
 //delete topics
-router.delete('/meetings/topics/:id',function(req,res,next){  console.log(req.body);
-  Meeting.findOneAndUpdate(
-
-         { _id: req.params.id },
-         { $pull: { topics: req.body}},
-         // !!!! note the { 'new': true } option
-         { 'new': true },
-         function(error, data) {
-           if(error){
-               res.json(error);
-           }
-           else{
-               res.json(data);
-           }
-         });
 
 
 //_______________________________________Task__________________________________
