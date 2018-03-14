0bb78606491e84d7d4580e7d8d381b0402434969
Jay Sheth <jsheth@my.bridgeport.edu>
2018-03-14 00:10:32 -0400

    commit errors solved
    ces

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,36 +1,15 @@
-<<<<<<< HEAD
-import { MeetingsService } from './meetings.service';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpModule } from '@angular/http';
-
-import { AppComponent } from './app.component';
-import { CreateMeetingComponent } from './meetings/create-meeting/create-meeting.component';
-import { HttpClient } from 'selenium-webdriver/http';
-import { ListMeetingComponent } from './meetings/list-meeting/list-meeting.component';
-import { AppRoutingModule } from './/app-routing.module';
-import { GetTaskComponent } from './task/listTask/get-task/get-task.component';
-import { Ng2FilterPipeModule } from 'ng2-filter-pipe';
-
-import { OrderByDoneAndDatePipe } from './order-by-done-and-date.pipe';
-=======
 import { MeetingsService } from "./meetings.service";
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpModule } from "@angular/http";
-import { TaskService } from "./task.service";
 import { AppComponent } from "./app.component";
 import { CreateMeetingComponent } from "./meetings/create-meeting/create-meeting.component";
 import { HttpClient } from "selenium-webdriver/http";
 import { ListMeetingComponent } from "./meetings/list-meeting/list-meeting.component";
 import { AppRoutingModule } from ".//app-routing.module";
 import { GetTaskComponent } from "./task/listTask/get-task/get-task.component";
-<<<<<<< HEAD
-import { ListTaskComponent } from "./task/listTask/list-task/list-task.component";
-import { DetailTaskComponent } from "./task/listTask/detail-task/detail-task.component";
-=======
->>>>>>> 0b6335a8e1e582ae6c69c1877e8b847fd53e313f
->>>>>>> 7080346615edc1ab757d0b3cb8b13fad55a66552
+import { OrderByDoneAndDatePipe } from "./order-by-done-and-date.pipe";
+import { TaskService } from "./task.service";
 
 @NgModule({
   declarations: [
@@ -38,21 +17,10 @@ import { DetailTaskComponent } from "./task/listTask/detail-task/detail-task.com
     CreateMeetingComponent,
     ListMeetingComponent,
     GetTaskComponent,
-   
     OrderByDoneAndDatePipe
   ],
-<<<<<<< HEAD
-  imports: [
-    BrowserModule,
-    HttpModule,
-    AppRoutingModule,
-    Ng2FilterPipeModule
-  ],
-  providers: [MeetingsService],
-=======
   imports: [BrowserModule, HttpModule, AppRoutingModule],
   providers: [MeetingsService, TaskService],
->>>>>>> 0b6335a8e1e582ae6c69c1877e8b847fd53e313f
   bootstrap: [AppComponent]
 })
 export class AppModule {}
