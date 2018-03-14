732334e328020d9f168a1d01faa34029022e7a59
trupa7 <35124558+trupa7@users.noreply.github.com>
2018-03-13 23:56:13 -0400

    aaa

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,19 +1,7 @@
-<<<<<<< HEAD
-import { MeetingsService } from './meetings.service';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpModule } from '@angular/http';
 
-import { AppComponent } from './app.component';
-import { CreateMeetingComponent } from './meetings/create-meeting/create-meeting.component';
-import { HttpClient } from 'selenium-webdriver/http';
-import { ListMeetingComponent } from './meetings/list-meeting/list-meeting.component';
-import { AppRoutingModule } from './/app-routing.module';
-import { GetTaskComponent } from './task/listTask/get-task/get-task.component';
 import { Ng2FilterPipeModule } from 'ng2-filter-pipe';
 
 import { OrderByDoneAndDatePipe } from './order-by-done-and-date.pipe';
-=======
 import { MeetingsService } from "./meetings.service";
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
@@ -25,7 +13,7 @@ import { HttpClient } from "selenium-webdriver/http";
 import { ListMeetingComponent } from "./meetings/list-meeting/list-meeting.component";
 import { AppRoutingModule } from ".//app-routing.module";
 import { GetTaskComponent } from "./task/listTask/get-task/get-task.component";
->>>>>>> 0b6335a8e1e582ae6c69c1877e8b847fd53e313f
+
 
 @NgModule({
   declarations: [
@@ -36,7 +24,7 @@ import { GetTaskComponent } from "./task/listTask/get-task/get-task.component";
    
     OrderByDoneAndDatePipe
   ],
-<<<<<<< HEAD
+
   imports: [
     BrowserModule,
     HttpModule,
@@ -44,10 +32,10 @@ import { GetTaskComponent } from "./task/listTask/get-task/get-task.component";
     Ng2FilterPipeModule
   ],
   providers: [MeetingsService],
-=======
+
   imports: [BrowserModule, HttpModule, AppRoutingModule],
   providers: [MeetingsService, TaskService],
->>>>>>> 0b6335a8e1e582ae6c69c1877e8b847fd53e313f
+
   bootstrap: [AppComponent]
 })
 export class AppModule {}
